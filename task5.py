8493d90e852baa45c02a6460b9e65966b162998e
Shubham Solanki <67789640+shubhamsolank24@users.noreply.github.com>
2020-07-20 23:12:29 +0530

    Update task5.py

--- task5.py
+++ task5.py
@@ -1,26 +1,17 @@
 class TrieNode:
     def __init__(self, end_of_word=False):
-        # Initialize this node in the Trie
+              self.end_of_word = end_of_word
 
-        # Indicates whether the string ends here is a valid word
-        self.end_of_word = end_of_word
-
-        # A dictionary to store the possible characters in this node
-        # Dictionary key: character (e.g. a)
-        # Dictionary value: pointer to child node
         self.char_dict = dict()
 
     def insert(self, char):
-        # Add a child node in this Trie
-        sub_char_node = TrieNode()
+         sub_char_node = TrieNode()
         self.char_dict[char] = sub_char_node
 
         return sub_char_node
 
     def suffixes(self, suffix=''):
-        # Recursive function that collects the suffix for
-        # all complete words below this point
-        output_str_list = list()
+    output_str_list = list()
 
         def find_suffix(node, output_str):
 
@@ -37,17 +28,14 @@ class TrieNode:
         return output_str_list
 
 
-# The Trie itself containing the root node and insert/find functions
+
 class Trie:
     def __init__(self):
-        # Initialize this Trie (add a root node)
+       
         root_node = TrieNode()
         self.root = root_node
 
     def insert(self, word):
-        # Add a word to the Trie
-
-        # Split the word into a seq of chars and build the corresponding TrieNodes
         cur_node = self.root
 
         for char in word:
@@ -57,14 +45,14 @@ class Trie:
                 new_child_node = cur_node.insert(char)
                 cur_node = new_child_node
 
-        # End of word, set end_of_word property to True
+       
         cur_node.end_of_word = True
 
     def find(self, prefix):
         # Find the Trie node that represents this prefix
 
         cur_node = self.root
-        # Traverse the Trie tree base on the character sequence in the prefix
+        # Traverse the Trie tree base on the character sequence.
         for char in prefix:
             if char in cur_node.char_dict:
                 cur_node = cur_node.char_dict[char]
