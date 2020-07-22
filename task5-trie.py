9d6a04a5ee6c43fdd23b04b53177dc6197c2c5b3
Shubham Solanki <67789640+shubhamsolank24@users.noreply.github.com>
2020-07-22 20:44:01 +0530

    Update and rename task5.py to task5-trie.py

--- task5-trie.py
+++ task5-trie.py
@@ -61,3 +61,51 @@ class Trie:
                 return None
 
         return cur_node
+class TrieNode:
+    def __init__(self, end_of_word=False):
+        self.end_of_word = end_of_word
+
+        self.char_dict = dict()
+    
+    def insert(self, char):
+        
+        sub_char_node = TrieNode()
+        self.char_dict[char] = sub_char_node
+
+        return sub_char_node
+        
+    def suffixes(self, suffix = ''):
+      
+        output_str_list = list()
+
+        def find_suffix(node, output_str):
+
+          
+            if node.end_of_word:
+                output_str_list.append(output_str)
+
+            for char in node.char_dict:
+                output_str += char
+                find_suffix(node.char_dict[char], output_str)
+
+        find_suffix(self, "")
+
+        return output_str_list
+    
+    
+MyTrie = Trie()
+wordList = [
+    "ant", "anthology", "antagonist", "antonym", 
+    "fun", "function", "factory", 
+    "trie", "trigger", "trigonometry", "tripod"
+    
+]
+for word in wordList:
+    MyTrie.insert(word)
+    
+    print(MyTrie)
+
+node = MyTrie.find('a')
+print(node.suffixes())
+    
+    
